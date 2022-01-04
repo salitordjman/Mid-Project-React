ae5b7082bd47070afb77660a1ed28da6e118d7aa
salitordjman <salitorgi@gmail.com>
2022-01-04 16:27:19 +0200

    navbar

--- src/App.js
+++ src/App.js
@@ -1,43 +1,25 @@
 import "./App.css";
-import { useRef } from "react";
-import color1 from "./img/color1.jpg";
-import color2 from "./img/color2.jpg";
-import black1 from "./img/black1.jpg";
-import black2 from "./img/black2.jpg";
-
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Sale from "./pages/Sale";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
 function App() {
-  const imgRef1 = useRef();
-  const imgRef2 = useRef();
-
   return (
-    <div className="all">
-      <div className="img1">
-        <img
-          src={black1}
-          alt="building"
-          ref={imgRef1}
-          onMouseOver={() => {
-            imgRef1.current.src = color1;
-          }}
-          onMouseOut={() => {
-            imgRef1.current.src = black1;
-          }}
-        />
-      </div>
-      <div className="img2">
-        <img
-          src={black2}
-          alt="building1"
-          ref={imgRef2}
-          onMouseOver={() => {
-            imgRef2.current.src = color2;
-          }}
-          onMouseOut={() => {
-            imgRef2.current.src = black2;
-          }}
-        />
-      </div>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Route path="/" exact component={Home} />
+        <Route path="/sale" exact component={Sale} />
+        <Route path="/profile" exact component={Profile} />
+        <Route path="/sign-in" exact component={SignIn} />
+        <Route path="/sign-up" exact component={SignUp} />
+        <Route path="/forgot-password" exact component={ForgotPassword} />
+      </Router>
+    </>
   );
 }
 
--- src/components/Starting.js
+++
@@ -1,5 +0,0 @@
-import "../App.css";
-
-const Starting = () => {};
-return <></>;
-export default Starting;
